35029c36db4e4d55b250ae926a1d2a301c644971
SvGOl <sv.golub88@gmail.com>
2025-06-05 16:53:04 -0600

    Add project Horoscope

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # HoroscopeAPI
-Demo page to work with Horoscope API 
+
+Demo page to work with Horoscope API
+
+# Using technologies
+
+HTML
+CSS
+JavaScript
+
+It's practical usage of Ninjas API to fetch horoscope for choosing zodiac sign
---
+++ script.js
@@ -0,0 +1,34 @@
+
+let selectedValue = null;
+const btns = document.querySelectorAll('.btn-signs');
+const divResult = document.querySelector('.result');
+
+for (let i = 0; i < btns.length; i++) {
+    let button = btns[i];
+    button.addEventListener('click', async function () {
+        selectedValue = button.value;
+        // console.log(selectedValue);
+        try {
+            const response = await fetch(`https://api.api-ninjas.com/v1/horoscope?zodiac=${selectedValue}`, {
+                headers: { 'X-Api-Key': 'A4CmyARivFLrKwIkWERXGQ==2dgU7cSZgsaEhBKl' },
+                method: "GET",
+                contentType: 'application/json'
+            });
+            if (response.ok) {
+                const data = await response.json();
+                // show result               
+                divResult.innerHTML =
+                    `<h2>${data.sign}</h2>    
+                    <span>${data.date}</span>
+                    <p>${data.horoscope}</p>`;
+                  
+                divResult.style.display = 'block';
+                
+                console.log(data);
+            } else {
+                throw new Error('Failed to fetch data');
+            }
+        } catch (error) {
+            console.error('Error:', error)
+        } })
+}; 
